257cf3475f09c8c2d8883ac02e3ff0c13233a4ba
“[Nicol <[nicol.saha@gmail.com]>
2020-07-30 13:31:37 +0200

    Fixed tailwind bug

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  plugins: [require("tailwindcss"), require("autoprefixer")],
+  plugins: [
+    require("postcss-import"),
+    require("tailwindcss"),
+    require("autoprefixer"),
+    /* require('@fullhuman/postcss-purgecss')({
+      content: [
+        './index.html', //Add all html files that use tailwind here
+        './other.html',
+      ],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    }), */
+  ],
 };
