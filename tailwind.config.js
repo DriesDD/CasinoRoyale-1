af701d7dc96b31b9a38ad35092de77f118de9243
DriesDD <driesdedecker@gmail.com>
2020-07-31 11:38:29 +0200

    removed custom colors again

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,7 @@ module.exports = {
     enabled: false,
     content: ['index.html', './game-one/game-one.html', './game-one/js-one.js', './game-two/game-two.html', './game-three/game-three.html', './game-four/game-four.html', ]
   },
-  theme: {
-    extend: {
-      colors: {
-        body: '#FBD38D',
-        banner: '#9B2C2C',
-        orange: '#F6AD55',
-        sunshine: '#FBD38D',
-        wild: '#FBD38D',
-        gold: '#C08E54',
-        classy: '#000000',
-      },
-    },
-  },
+  theme: {},
   variants: {},
   plugins: [],
 }
