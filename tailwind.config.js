63258526f56f4c9bae029532544fb115f5d3d255
DriesDD <driesdedecker@gmail.com>
2020-07-31 11:18:53 +0200

    add theme colors to config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 module.exports = {
   purge: {
     enabled: false,
-    content: ['index.html','./game-one/game-one.html','./game-one/js-one.js','./game-two/game-two.html','./game-three/game-three.html','./game-four/game-four.html',]
+    content: ['index.html', './game-one/game-one.html', './game-one/js-one.js', './game-two/game-two.html', './game-three/game-three.html', './game-four/game-four.html', ]
   },
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        body: '#FBD38D',
+        banner: '#9B2C2C',
+        orange: '#F6AD55',
+        sunshine: '#FBD38D',
+        wild: '#FBD38D',
+        gold: '#C08E54',
+        classy: '#000000',
+      },
+    },
   },
   variants: {},
   plugins: [],
