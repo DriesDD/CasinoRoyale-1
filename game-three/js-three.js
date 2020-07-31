289b02fee924a5c50fb2f0e97549a083d1d1d7c5
“[Nicol <[nicol.saha@gmail.com]>
2020-07-31 15:01:37 +0200

    Worked on Game 3

--- game-three/js-three.js
+++ game-three/js-three.js
@@ -1,5 +1,46 @@
-const cards = document.querySelectorAll(".memory-card");
+const cards = document.querySelectorAll(".memory.cards");
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
-  this.classList.toggle("flip");
+  cards.className += "flip";
+
+  if (!hasFlippedCard) {
+    // first click
+    hasFlippedCard = true;
+    firstCard = this;
+
+    return;
+  }
+
+  // second click
+  hasFlippedCard = false;
+  secondCard = this;
+
+  checkForMatch();
 }
+
+function checkForMatch() {
+  if (firstCard.dataset.image === secondCard.dataset.image) {
+    // match
+    disableCards();
+  } else {
+    // no match
+    unflipCard();
+  }
+}
+
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+}
+
+function unflipCard() {
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+  }, 1500);
+}
+
 cards.forEach((card) => card.addEventListener("click", flipCard()));
