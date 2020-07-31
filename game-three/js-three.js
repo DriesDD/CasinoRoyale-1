db7e1addfe691a1e2b74765287c3657d46e26875
“[Nicol <[nicol.saha@gmail.com]>
2020-07-31 15:22:27 +0200

    Worked on Game 3 Bug = cards not staying put

--- game-three/js-three.js
+++ game-three/js-three.js
@@ -1,9 +1,13 @@
-const cards = document.querySelectorAll(".memory.cards");
+const cards = document.querySelectorAll(".memory.card");
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
   cards.className += "flip";
 
   if (!hasFlippedCard) {
@@ -15,9 +19,7 @@ function flipCard() {
   }
 
   // second click
-  hasFlippedCard = false;
   secondCard = this;
-
   checkForMatch();
 }
 
@@ -34,13 +36,30 @@ function checkForMatch() {
 function disableCards() {
   firstCard.removeEventListener("click", flipCard);
   secondCard.removeEventListener("click", flipCard);
+
+  resetBoard();
 }
 
 function unflipCard() {
+  lockBoard = true;
   setTimeout(() => {
     firstCard.classList.remove("flip");
     secondCard.classList.remove("flip");
+
+    resetBoard();
   }, 1500);
 }
 
-cards.forEach((card) => card.addEventListener("click", flipCard()));
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach((card) => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
+
+cards.forEach((card) => card.addEventListener("click", flipCard));
