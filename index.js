37f06b68de8bcfcd29cdfa2b8f4f18d5a7a3bfa6
“[Nicol <[nicol.saha@gmail.com]>
2020-08-05 14:59:03 +0200

    Added flyout menu

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-document.querySelector(`#id`).classList.add("bg-red-500");
-document.querySelector(`#id`).classList.remove("bg-red-500");
+// Flyout Menu
 
-show = { isOpen };
-enter = "transition ease-out duration-200";
-enterFrom = "opacity-0 translate-y-1";
-enterTo = "opacity-100 translate-y-0";
-leave = "transition ease-in duration-150";
-leaveFrom = "opacity-100 translate-y-0";
-leaveTo = "opacity-0 translate-y-1";
+const menu = document.getElementById("menuFlyoutBtn");
+const flyoutmenu = document.querySelector("#flyoutmenu");
+
+function toggleMenu() {
+  flyoutmenu.classList.remove("opacity-0", "-translate-y-1");
+  flyoutmenu.classList.add(
+    "transition ease-out",
+    "transition duration-200",
+    "opacity-100",
+    "-translate-y-0"
+  );
+}
+
+function toggleMenuOff() {
+  flyoutmenu.classList.add("opacity-0", "-translate-y-1");
+  flyoutmenu.classList.remove(
+    "transition ease-out",
+    "transition duration-200",
+    "opacity-100",
+    "-translate-y-0"
+  );
+}
+
+menu.addEventListener("mouseenter", toggleMenu);
+flyoutmenu.addEventListener("mouseleave", toggleMenuOff);
