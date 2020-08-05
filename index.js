214e0e70be39bdb313cf6dd3a3d52a937ff26f5f
“[Nicol <[nicol.saha@gmail.com]>
2020-08-05 12:09:52 +0200

    Worked on flyout menu

---
+++ index.js
@@ -0,0 +1,10 @@
+document.querySelector(`#id`).classList.add("bg-red-500");
+document.querySelector(`#id`).classList.remove("bg-red-500");
+
+show = { isOpen };
+enter = "transition ease-out duration-200";
+enterFrom = "opacity-0 translate-y-1";
+enterTo = "opacity-100 translate-y-0";
+leave = "transition ease-in duration-150";
+leaveFrom = "opacity-100 translate-y-0";
+leaveTo = "opacity-0 translate-y-1";
